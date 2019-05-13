447ce2312f0265799b1398a02f0a133c2fc8aade
Chris Tso <christophertso2017@u.northwestern.edu>
2019-05-13 16:30:04 -0500

    Set up server

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+var fs = require('fs');
+var express = require('express');
+var app = express(); 
+var hbs = require('hbs');
+hbs.registerPartials(__dirname + '/views/partials');
 
+var port = 3000; 
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('login');
+});
+
+app.listen(port, () => {console.log('listening at PORT 3000')});
