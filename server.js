d4e50dac43106b13db40bba0e097b8dd6c756852
Chris Tso <christophertso2017@u.northwestern.edu>
2019-05-13 19:12:46 -0500

    Added saving users to database

--- server.js
+++ server.js
@@ -1,20 +1,37 @@
-var fs = require('fs');
 var express = require('express');
 var app = express(); 
 var hbs = require('hbs');
+var bodyParser = require('body-parser');
+var db = require('./database.js');
+
+
 hbs.registerPartials(__dirname + '/views/partials');
 
 var port = 3000; 
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
     res.render('login', {title: 'Registration'});
 });
 
 app.post('/login', (req, res) => {
-    res.render('login', {title: 'Registration Complete'});
+    var username =  req.body.username;
+    var email = req.body.email;
+    var password = req.body.password;
+    
+    db.saveUser(username, email, password, (err, data) => {
+        if(err){
+            console.log(err);
+            return;
+        }
+        res.render('login', {title: 'Registration Complete'});
+
+    })
+
 });
 
 app.listen(port, () => {console.log('listening at PORT 3000')});
